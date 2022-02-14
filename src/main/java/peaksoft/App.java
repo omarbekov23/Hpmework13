8fa0e169cb653845d44627d2675fa8703c24f319
erlan <eomabekov@gmail.com>
2022-02-14 12:23:22 +0300

    I have completed this assignment!

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -7,8 +7,19 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld helloWorld =  applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(helloWorld.getMessage());
+        System.out.println(helloWorld==bean);
+        System.out.println(bean.equals(helloWorld));
+
+        Cat one = applicationContext.getBean("cat", Cat.class);
+        System.out.println(one);
+        Cat two = (Cat) applicationContext.getBean("cat", Cat.class);
+        System.out.println(two);
+        System.out.println(one==two);
+        System.out.println(two.equals(one));
+
     }
 }
---
+++ src/main/java/peaksoft/Cat.java
@@ -0,0 +1,49 @@
+package peaksoft;
+
+public class Cat {
+private String name;
+private int age;
+private String gender;
+
+    public Cat() {
+    }
+
+    public Cat(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
